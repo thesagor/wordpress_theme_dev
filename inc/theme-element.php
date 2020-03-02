01a9d06de5e679d26b77f4005712a739e356e056
thesagor <imthesagor@gmail.com>
2020-03-02 16:12:14 +0600

    raw code added without help

--- inc/theme-element.php
+++ inc/theme-element.php
@@ -1,80 +1,75 @@
-<?php
-function myvcshort($atts,$content=null){
+<?php 
 
-    extract(shortcode_atts(
-        array(
-            'themex_title'=>'',
-            'themex_images'=> '',
-            'themex_title_color'=>'',
-            'themex_desc' => '',
-            'themex_group' => array(
-                'group_title'=>'',
-                'group_amount'=>'',
-                'group_amount_color'=>''
+    function myShortcode($atts,$content = null){
+        extract(shortcode_atts(array(
+            'first_title'=>'',
+            'first_desc'=>'',
+            'first_title_color'=>'',
+            'first_section_image'=>'',
+            'themexgroup'=> array(
+                'progress_title'=> '',
+                'progress_amount_color'=> '',
+                'progress_amount'=> ''
+
             )
-        ),$atts)
-    );
 
-    ob_start();?>
+        ),$atts));
+        ob_start();?>
 
-        <div class="get_in_tuch_skill">
-            <div class="container-fluid nopadding">
-                <div class="col-md-6 col-sm-6 padding_left">
-                    <div class="left_image_get_in">
-                        <!-- <img src="images/home/digital_home_skill_get_in_tuch.jpg" alt=""> -->
+            <div class="get_in_tuch_skill">
+                <div class="container-fluid nopadding">
+                    <div class="col-md-6 col-sm-6 padding_left">
+                        <div class="left_image_get_in">
+
                         <?php
-                            $img_atts = wp_get_attachment_image_src($themex_images, 'full');
-                        ?>
-                        <img src="<?php echo $img_atts[0]; ?>" alt="">
-
-                    </div>
-                </div><!-- col-md-6  -->
-
-                <div class="col-md-3 col-sm-6">
-                    <div class="middle_skill_get_in heroo_get_in_tuch">
-                        <div class="middle_skill_get_in_cell">
-                            <div class="hero-progress">
-                            <?php $themexGroup = vc_param_group_parse_atts( $atts['themex_group'] );
-                                if($themexGroup){
-                                    foreach ($themexGroup as $key => $value) { ?>
-                                        <div class="progress-item">
-                                            <p><?php echo esc_html($value['group_title']);?><span class="pull-right"><?php echo esc_html($value['group_amount']);?></span></p>
-                                            <div class="progress-main"><span class="progress-bar" style="background-color:<?php echo esc_html($value['group_amount_color']);?>"  data-value="<?php echo esc_html($value['group_amount']);?>%" ></span></div>
-                                        </div><!-- progress-item  -->
-                                        
-                                   <?php }
-                                }                            
-                            
-                            ?>
-
-
-                                
-
-
-                                
-                            </div>
-                        </div><!-- middle_skill_get_in_cell -->
-                    </div><!-- middle_skill_get_in -->
-                </div><!-- col-md-3  -->
-
-                <div class="col-md-3 padding_right col-sm-6">
-                    <div class="right_skill_get_in_text">
-                        <div class="right_skill_get_in_text_cell">
-                            <h3 style="color:<?php echo esc_attr($themex_title_color);?>"> <?php echo esc_html($themex_title);?></h3>
-                            <p><?php echo esc_html($themex_desc);?></p>
-                            <a href="#" class="MESSAGE_uS">MESSAGE US</a>
+                             $image_attributes = wp_get_attachment_image_src( $first_section_image,'full ' );
+                            if ( $image_attributes ) : ?>
+                                <img src="<?php echo $image_attributes[0]; ?>" />
+                            <?php endif; ?>
                         </div>
-                    </div>
-                </div><!-- col-md-3  -->
-            </div><!-- container-fluid  -->
-        </div><!-- get_in_tuch_skill  -->
+                    </div><!-- col-md-6  -->
+
+                    <div class="col-md-3 col-sm-6">
+                        <div class="middle_skill_get_in heroo_get_in_tuch">
+                            <div class="middle_skill_get_in_cell">
+                                <div class="hero-progress">
+
+                                    <?php
+                                        $vc_group_themex = vc_param_group_parse_atts( $atts['themexgroup'] );
+                                        if($vc_group_themex){
+                                            foreach ($vc_group_themex as $key => $value) { ?>
+                                                <div class="progress-item">
+                                                    <p><?php echo esc_html( $value['progress_title'] )?><span class="pull-right"><?php echo esc_html( $value['progress_amount'] )?>%</span></p>
+                                                    <div class="progress-main"><span class="progress-bar" style="background-color:<?php echo esc_html( $value['progress_amount_color'] )?>" data-value="<?php echo esc_html( $value['progress_amount'] )?>%"></span></div>
+                                                </div><!-- progress-item  -->
+
+                                    <?php  } } ?>
+                                    
+                                </div>
+                            </div><!-- middle_skill_get_in_cell -->
+                        </div><!-- middle_skill_get_in -->
+                    </div><!-- col-md-3  -->
+
+                    <div class="col-md-3 padding_right col-sm-6">
+                        <div class="right_skill_get_in_text">
+                            <div class="right_skill_get_in_text_cell">
+                                <h3 style="color:<?php echo esc_attr( $first_title_color )?>"><?php echo esc_html( $first_title )?></h3>
+                                <p><?php echo esc_html( $first_desc )?></p>
+                                <a href="#" class="MESSAGE_uS">MESSAGE US</a>
+                            </div>
+                        </div>
+                    </div><!-- col-md-3  -->
+                </div><!-- container-fluid  -->
+            </div><!-- get_in_tuch_skill  -->
 
 
-    <?php 
-        return ob_get_clean();
 
-}
-add_shortcode('myvcshortcode','myvcshort')
+        <?php return ob_get_clean();
+
+
+    }
+    add_shortcode('myVcShortcode','myShortcode');
+    
 
 
 ?>
--- inc/theme-options.php
+++ inc/theme-options.php
@@ -1,71 +1,66 @@
 <?php
-
-    function myVcmap(){
-        vc_map( array(
-            "name" => __( "Themex Addon", "themex" ),
-            "base" => "myvcshortcode",
-            "description" => "This Addon is available only for Themex",
-            "category" => __( "Themex", "themex"),
-            'icon' => get_template_directory_uri().'/images/logo-x.png',
-            "params" => 
+    function VCmapAddon(){
+        vc_map(array(
+            'base'=>'myVcShortcode',
+            'name'=>'Themex Hero Setion',
+            'category'=>'ThemeX',
+            'params'=>array(
                 array(
-                    array(                    
-                        "type" => "textfield",
-                        "heading" => __( "Please add 3rd section Title", "themex" ),
-                        "param_name" => "themex_title",
-                        "value" => __( "This is test param for creating new project", "themex" ),
-                        "description" => __( "This is title zone.", "themex" )
-                    ),
-                    array(                    
-                        "type" => "colorpicker",
-                        "heading" => __( "Please add color for Title", "themex" ),
-                        "param_name" => "themex_title_color",
-                        "value" => __( "This is test param for creating new project", "themex" ),
-                        "description" => __( "This is title zone.", "themex" )
-                    ),
-                    array(                    
-                        "type" => "attach_image",
-                        "heading" => __( "Please Add Images For First section", "themex" ),
-                        "param_name" => "themex_images",
-                        "value" => __( "This is default value for images", "themex" ),
-                        "description" => __( "This is title zone.", "themex" )
-                    ),
-                    array(                    
-                        "type" => "textarea",
-                        "heading" => __( "Please Add Text box For 3rd section", "themex" ),
-                        "param_name" => "themex_desc",
-                        "value" => __( "This is default value for images", "themex" ),
-                        "description" => __( "This is title zone.", "themex" )
-                    ),
-                    array(                    
-                        "type" => "param_group",
-                        "heading" => __( "Prgress Bar", "themex" ),
-                        "param_name" => "themex_group",
-                        "params"=>array(
-                            array(
-                                "type" => "textfield",
-                                "heading" => __( "Title", "themex" ),
-                                "param_name" => "group_title"
-                            ),
-                            array(
-                                "type" => "textfield",
-                                "heading" => __( "Progress Amount", "themex" ),
-                                "param_name" => "group_amount"
-                            ),
-                            array(
-                                "type" => "colorpicker",
-                                "heading" => __( "Progress Amount color", "themex" ),
-                                "param_name" => "group_amount_color"
-                            )
+                    'type'=>'textfield',
+                    'param_name'=>'first_title',
+                    'heading'=>'This is Title Section'
+                ),
+                array(
+                    'type'=>'textarea',
+                    'param_name'=>'first_desc',
+                    'heading'=>'This is Desctinon 3rd Section'
+                ),
+                array(
+                    'type'=>'colorpicker',
+                    'param_name'=>'first_title_color',
+                    'heading'=>'This is Desctinon 3rd Section title color'
+                ),
+                array(
+                    'type'=>'attach_image',
+                    'param_name'=>'first_section_image',
+                    'heading'=>'This is First setion image '
+                ),
+                
+                  // params group
+                array(
+                    'type' => 'param_group',
+                    'value' => '',
+                    'group'=>'Progress Bar',
+                    'param_name' => 'themexgroup',
+                    // Note params is mapped inside param-group:
+                    'params' => array(
+                        array(
+                        'type' => 'textfield',
+                        'value' => '',
+                        'heading' => 'Enter your title(multiple field)',
+                        'param_name' => 'progress_title',
+                        ),
+                        array(
+                            'type' => 'textfield',
+                            'value' => '',
+                            'heading' => 'Enter your title(multiple field)',
+                            'param_name' => 'progress_amount',
+                        ),
+                        array(
+                            'type' => 'colorpicker',
+                            'value' => '',
+                            'heading' => 'Enter your title(multiple field)',
+                            'param_name' => 'progress_amount_color',
                         )
                     )
-
                 )
 
+                
+                
             )
-        );
+
+            ));
+
     }
-    add_action('vc_before_init','myVcmap');
-
-
+    add_action('vc_before_init','VCmapAddon');
 ?>
